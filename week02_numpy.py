4a97d2bd2a53effb42fb1ef3b588e919309cd223
JoRaebab <116784565+JoRaebab@users.noreply.github.com>
2023-09-07 16:17:55 +0900

    week02 v0.6

--- week02_numpy.py
+++ week02_numpy.py
@@ -9,7 +9,7 @@ print(v)
 #v = np.array([1, 3, -9, 2])
 #v = np.array([1, 3, -9, 2], dtype='int64')
 #v = np.array([[1, 3, -9, 2], [71, 13, -29, 7]])#, dtype='int64')
-#print(v.ndim, v.shape, v.data, v.dtype, v.strides)
+# print(v.ndim, v.shape, v.data, v.dtype, v.strides)
 
 
 
