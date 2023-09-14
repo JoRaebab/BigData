74c382a2436cb55d3cfc79d8caa947813ca17c20
201930233 <buzzi123@naver.com>
2023-09-14 15:22:23 +0900

    week03 v1.1 2d array

--- week03_numpy.py
+++ week03_numpy.py
@@ -5,10 +5,15 @@ from tkinter import messagebox
 
 def click_button():
     try:
-        n = int(en_number.get())
-        l = [random.randint(1, 100) for i in range(n)]
-        v = np.array(l, dtype='int16')
-        lbl_result.config(text=v)
+        r = int(en_row.get())
+        c = int(en_col.get())
+
+        rows= list()
+        for i in range(c):
+            rows.append([random.randint(1, 100) for i in range(r)])
+        # print(rows)
+        matrix = np.array(rows, dtype='int16')
+        lbl_result.config(text=matrix)
     except ValueError as err:
         lbl_result.config(text=f"입력 값이 없습니다.\n{err}")
         messagebox.showerror('Error!',f"입력 값이 없습니다.\n{err}")
@@ -16,20 +21,26 @@ def click_button():
 
 
 window = tk.Tk()
-window.title('numpy gui version v0.7')
+window.title('numpy gui version v1.1')
 window.geometry('300x150')
 
 # create widget
-lbl_result = tk.Label(text="random numpy array")
-en_number = tk.Entry()
+lbl_result = tk.Label(text="random numpy 2d array")
+en_row = tk.Entry()
+en_col = tk.Entry()
 btn_click = tk.Button(text="click me!", command=click_button)
 
 # widget layout
-lbl_result.place(x=50, y=50)
-btn_click.place(x=0, y=0)
-# lbl_result.grid(row=0, column=0)
-# en_number.grid(row=1, column=0)
-# btn_click.grid(row=1, column=1)
+# lbl_result.place(x=50, y=50)
+# btn_click.place(x=0, y=0)
+lbl_result.grid(row=0, column=0, columnspan=2)
+en_row.grid(row=1, column=0)
+en_col.grid(row=1, column=1)
+btn_click.grid(row=2, column=0, columnspan=2, sticky=tk.EW)
+# lbl_result.pack()
+# en_row.pack(fill='x')
+# en_col.pack(fill='x')
+# btn_click.pack(fill='x')
 
 window.mainloop()
 # n = int(input("input number : "))
