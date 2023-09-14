0ece070b37f89c4f067cc4c123b6e3f325f8869e
201930233 <buzzi123@naver.com>
2023-09-14 14:48:03 +0900

    week03 v0.8 exception handling

---
+++ week03_numpy.py
@@ -0,0 +1,36 @@
+import numpy as np
+import random
+import tkinter as tk  # Built in GUI
+from tkinter import messagebox
+
+def click_button():
+    try:
+        n = int(en_number.get())
+        l = [random.randint(1, 100) for i in range(n)]
+        v = np.array(l, dtype='int16')
+        lbl_result.config(text=v)
+    except ValueError as err:
+        lbl_result.config(text=f"입력 값이 없습니다.\n{err}")
+        messagebox.showerror('Error!',f"입력 값이 없습니다.\n{err}")
+
+
+
+window = tk.Tk()
+window.title('numpy gui version v0.7')
+window.geometry('300x150')
+
+# create widget
+lbl_result = tk.Label(text="random numpy array")
+en_number = tk.Entry()
+btn_click = tk.Button(text="click me!", command=click_button)
+
+# widget layout
+lbl_result.grid(row=0, column=0)
+en_number.grid(row=1, column=0)
+btn_click.grid(row=1, column=1)
+
+window.mainloop()
+# n = int(input("input number : "))
+# l = [random.randint(1, 100) for i in range(n)]
+# v = np.array(l, dtype='int16')
+# print(v)
