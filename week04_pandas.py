1227dfe4aebdaca57e823ab5e2f98d959a01aac6
201930233 <buzzi123@naver.com>
2023-09-21 16:20:59 +0900

    week04 v2.1 pandas

--- week04_pandas.py
+++ week04_pandas.py
@@ -1,30 +1,35 @@
+import tkinter as tk
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-# csv file download and data loading
-life_satisfaction = pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv")
-# print(life_satisfaction.head(5))
-# print(life_satisfaction.shape)
-# print(life_satisfaction.describe())
-
-X = life_satisfaction[["GDP per capita (USD)"]].values # return 2d array
-y = life_satisfaction[["Life satisfaction"]].values # return 2d array
-# print(X,y)
+def predict_life_satisfaction() :
+    x = int(en_GDP_per_capita.get())
+    X_new = [[x]]
 
 
-# draw scatter diagram
-life_satisfaction.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction")
-plt.axis([23500, 62500, 4, 9]) # x의 최소, 최대값, y의 최소,최대값
-plt.show()
+    life_satisfaction = pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv")
 
-# model choice
-model = LinearRegression()
 
-# Train the model
-model.fit(X, y)
+    X = life_satisfaction[["GDP per capita (USD)"]].values  # return 2d array
+    y = life_satisfaction[["Life satisfaction"]].values  # return 2d array
 
-# predict new GDP per capita (Cyprus 2020)
-X_new = [[31700.2]]
-print(life_satisfaction)
-print(model.predict(X_new))
+    model = LinearRegression()
+    model.fit(X, y)
+
+    lbl_life_satisfaction.config(text=f"해당 국가의 삶의 만족도는{model.predict(X_new)}")
+
+if __name__ == "__main__":
+    window = tk.Tk()
+    window.title("삶의 만족도 예측 프로그램 v0.1")
+    window.geometry("400x150")
+
+    lbl_life_satisfaction = tk.Label(text="아래 입력상자에 삶의 만족도를 알고 싶은\n국가의 1인당 GDP를 입력해주세요.")
+    en_GDP_per_capita = tk.Entry(window)
+    btn_predict = tk.Button(window, text="예측", command=predict_life_satisfaction)
+
+    lbl_life_satisfaction.pack()
+    en_GDP_per_capita.pack(fill="x")
+    btn_predict.pack(fill="x")
+
+    window.mainloop()
