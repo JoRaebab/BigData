314a5b931d31bc7df4c26014ebb9dab43c8d0cb2
201930233 <buzzi123@naver.com>
2023-10-12 14:11:56 +0900

    week06 v2.4 module&class

--- week06_class_module_kneighbor.py
+++ week06_class_module_kneighbor.py
@@ -1,17 +1,27 @@
+# import matplotlib.pyplot as plt
 import pandas as pd
 import tkinter as tk
+from sklearn.neighbors import KNeighborsRegressor
 import tglearn
 
 
-def predict_life_satisfaction():
-    x = int(en_GDP_per_capita.get())  # scalar input
+def predict_life_satisfaction(*ev):
+    x = int(en_GDP_per_capita.get())
     X_new = [[x]]
 
     life_satisfaction = pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv")
     X = life_satisfaction[["GDP per capita (USD)"]].values  # return 2d array
     y = life_satisfaction[["Life satisfaction"]].values  # return 2d array
 
-    model = tglearn.LinearRegression()
+    # print(X)
+    # print(life_satisfaction)
+
+    # life_satisfaction.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction")
+    # plt.axis([23500, 62500, 4, 9])
+    # plt.show()
+
+    model = tglearn.KNeighborsRegressor()  # default neighbor is 5
+    #model = KNeighborsRegressor()
     model.fit(X, y)
 
     # predict new GDP per capita (South Korea 2020)
@@ -20,7 +30,7 @@ def predict_life_satisfaction():
 
 if __name__ == "__main__":
     window = tk.Tk()
-    window.title("삶의 만족도 예측 프로그램 v0.4")
+    window.title("삶의 만족도 예측 프로그램 v0.6")
     window.geometry("400x150")
 
     lbl_life_satisfaction = tk.Label(window, text="아래 입력상자에 삶의 만족도를 알고 싶은\n국가의 1인당 GDP값을 입력해주세요")
@@ -30,5 +40,8 @@ if __name__ == "__main__":
     lbl_life_satisfaction.pack()
     en_GDP_per_capita.pack(fill='x')
     btn_predict.pack(fill='x')
+
+    en_GDP_per_capita.bind("<Return>", predict_life_satisfaction)
+    en_GDP_per_capita.focus()
 
     window.mainloop()
