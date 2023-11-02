677930f85ec61c5bc8f5aec761450add01168b8c
201930233 <buzzi123@naver.com>
2023-11-02 16:14:43 +0900

    week10 v4.8 numpy & pandas

--- week10_numpy_review.py
+++ week10_numpy_review.py
@@ -1,27 +1,41 @@
 import numpy as np
 
-a1 = np.arange(1, 6)
-a2 = np.array([6, 7, 8, 9, 10])
-a3 = np.array([
-    [3, 2, 1],
-    [-11, 0, 9]
+a = np.arange(1, 7).reshape(2, 3)
+b = np.array([
+    [5, 20],
+    [11, -1],
+    [9, 3]
 ])
-print(a1 + a2)
-print(a1 - a2)
-print(a1 * a2)
-print(a1 / a2)
-print(np.dot(a1, a2))
-# a3 = a1 + a2
-# print(a3)
-print(np.sum(a1) + np.sum(a2))
-print(np.prod(a1), np.prod(a2))
-# by aixs
-print(a3.sum(axis=0))
-print(a3.sum(axis=1))
-print(a3.prod(axis=0))
-print(a3.prod(axis=1))
-print(a3.mean()) # 산술 평균
-print(a3.std()) # 표준 편차
+print(a.dot(b))
+print(np.dot(a, b))
+print(b.dot(a))
+print(np.dot(b, a))
+
+
+
+# a1 = np.arange(1, 6)
+# a2 = np.array([6, 7, 8, 9, 10])
+# a3 = np.array([
+#     [3, 2, 1],
+#     [-11, 0, 9]
+# ])
+# print(a1 + a2)
+# print(a1 - a2)
+# print(a1 * a2)
+# print(a1 / a2)
+# print(np.dot(a1, a2))
+# # a3 = a1 + a2
+# # print(a3)
+# print(np.sum(a1) + np.sum(a2))
+# print(np.prod(a1), np.prod(a2))
+# # by aixs
+# print(a3.sum(axis=0))
+# print(a3.sum(axis=1))
+# print(a3.prod(axis=0))
+# print(a3.prod(axis=1))
+# print(a3.mean()) # 산술 평균
+# print(a3.std()) # 표준 편차
+
 
 # a1 = np.ones((3, 3))
 # a1[:, 2] = 2
