09df45cb9335c5470d338c3d1380bed636a1790e
201930233 <buzzi123@naver.com>
2023-11-09 16:30:26 +0900

    week11 v5.9 numpy & pandas X

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -9,16 +9,46 @@ df = pd.DataFrame(
     columns=['KOR', 'ENG', 'MAT']
 )
 
-# def scale_score(n):
-#     return n + 1
+scores = [
+    [99, 89, 81],
+    [91, 98, 90],
+    [95, 97, 85],
+    [83, 96, 94]
+]
+index = [1, 2, 3, 4]
+columns = ['KOR', 'ENG', 'MAT']
+
+for c in columns:
+    print(f'{c}', end=' ')
+print()
+for i in range(4):
+    print(index[i], end=' ')
+    for j in range(3):
+        print(scores[i][j], end=" ")
+        scores[i][j] = scores[i][j]+1
+    print()
+
+print()
+
+for c in columns:
+    print(f'{c}', end=' ')
+print()
+for i in range(4):
+    print(index[i], end=' ')
+    for j in range(3):
+        print(scores[i][j], end=" ")
+    print()
+
+def scale_score(n):
+    return n + 1
 #
 # print(df)
 # df = df.apply(scale_score)
 # print(df)
 
-print(df)
-df = df.apply(lambda n: n+1)
-print(df)
+# print(df)
+# df = df.apply(lambda n: n+1)
+# print(df)
 
 #국어, 수학 칼럼을 추출
 # 조건은 국어 성적이 95점 이상인 경우
