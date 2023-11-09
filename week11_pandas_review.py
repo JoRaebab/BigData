f0a854784a47af1c9f5aee71ffc1060070ff5035
201930233 <buzzi123@naver.com>
2023-11-09 16:05:14 +0900

    week11 v5.8 numpy & pandas

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -8,24 +8,30 @@ df = pd.DataFrame(
     index=[1, 2, 3, 4],
     columns=['KOR', 'ENG', 'MAT']
 )
-print(df.mean()) # 평균 출력
-print(df.max()) # 최대 값 출력16456
-print(df)
+# print(df.mean()) # 평균 출력
+# print(df.max()) # 최대 값 출력
+# print(df)
 # df_copy = df.iloc[:, [0, 2]]
 # df_copy = df.loc[:, ['KOR', 'MAT']]
 # df_copy = df.loc[:, 'KOR':'MAT':2] #loc는 마지막으로 지정한 항목까지 포함해서 출력
 
 #국어, 수학 칼럼을 추출
 # 조건은 국어 성적이 95점 이상인 경우
-df_copy = df.loc[df['KOR'] >= 95, ['KOR', 'MAT']]
-print(df_copy)
+# df_copy = df.loc[df['KOR'] >= 95, ['KOR', 'MAT']]
+# print(df_copy)
+
+def scale_score(n):
+    return n + 1
+
+df = df.apply(scale_score)
+print(df)
 
 #1번 학생의 수학성적(100) 출력
-print(df.at[1, 'MAT'])
-print(df.iat[0, 2]) #iat은 index로 사용하기 때문에 loc처럼 행열을 가져오지 않음
+# print(df.at[1, 'MAT'])
+# print(df.iat[0, 2]) #iat은 index로 사용하기 때문에 loc처럼 행열을 가져오지 않음
 
 # 국어, 영어 성적 둘 다 95점 이상인 경우
-print(df.query('KOR>=95 and ENG>=95'))
+# print(df.query('KOR>=95 and ENG>=95'))
 
 # df1 = pd.DataFrame(
 #     {
