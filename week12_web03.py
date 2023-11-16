99786aa4dfb74d377ea9153cdf2289c937933416
201930233 <buzzi123@naver.com>
2023-11-16 16:34:36 +0900

    week12 v6.5 web

--- week12_web03.py
+++ week12_web03.py
@@ -1,35 +1,26 @@
+import urllib.request
 from bs4 import BeautifulSoup
+import pandas as pd
+import datetime
 
-html = """
-<html>
-<head>
-<title>스크레이핑 실습</title>
-</head>
-<body>
-<h1 id = "univ">대림대학교</h1>
-<p>웹스크레이핑</p>
-<p id = "contents">파이썬, 판다스, 넘파이, 맷플롯립, GUI...</p>
-</body>
-</html>
-"""
+shops = list()
+for i in range(1, 51):
+    url = f"https://www.hollys.co.kr/store/korea/korStore2.do?pageNo={i}&sido=&gugun=&store="
+    print(url)
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser') #html.parser는 기본값
+    tbody = soup.find('tbody')
+    trs = tbody.find_all('tr') # tr list
 
-soup = BeautifulSoup(html, 'html.parser')
-university = soup.find(id='univ')
-contents = soup.find(id='contents')
+    for tr in trs:
+        tds = tr.find_all('td')
+        shop_name = tds[1].string # 매장 이름
+        shop_addr = tds[3].string # 매장 주소
+        shop_tel = tds[5].string # 매장 전화번호
 
-print(university)
-print(contents.string)
-print(contents)
+        shops.append([shop_name] + [shop_addr] + [shop_tel] + [datetime.datetime.now()])
 
+#print(shops)
+hollys_df = pd.DataFrame(shops, columns=('매장명', '주소', 'Tel', '일시'))
+hollys_df.to_csv('hollys.csv', mode='w', encoding='cp949')
 
-# soup = BeautifulSoup(html, 'html.parser')
-# t = soup.html.head.title
-# print(t)
-# print(t.string)
-# h1 = soup.html.body.h1.string
-# print(h1)
-# p1 = soup.html.body.p.string
-# #p2 = soup.html.body.p.string
-# p2 = p1.next_sibling.next_sibling
-# print(p1)
-# print(p2)
